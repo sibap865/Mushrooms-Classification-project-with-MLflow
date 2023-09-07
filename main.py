27a04eef8e5177b5a3694092eafdfe5006f1865a
sibaprasad <sibap865@gmail.com>
2023-09-07 22:31:53 +0530

    data valifdation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from MushroomProject import logger
 from MushroomProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from MushroomProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,3 +13,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
--- src/MushroomProject/config/configuration.py
+++ src/MushroomProject/config/configuration.py
@@ -1,7 +1,7 @@
 from MushroomProject.constants import *
 from MushroomProject.utils.common import read_yaml, create_directories
 
-from MushroomProject.entity.config_entity import (DataIngestionConfig)
+from MushroomProject.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -32,3 +32,17 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config   
